7b53d7073e8456efbccfe184f1f069e301510751
Sid1057 <ivanov.dale@gmail.com>
2018-12-12 02:33:11 +0300

    new version. cards updates.

--- index.js
+++ index.js
@@ -4,6 +4,22 @@ Vue.use(VueMaterial.default)
 new Vue({
 	el: '#app',
 	data: {
-		data: "hey ho!"
+		content: {
+			news0: {
+				id: 0,
+				title: 'We took the first step',
+				subhead: '2018/01/01',
+				content: 'Work has begun. The first template of our site is complete. Here we will write news and publish projects about the Earth, the Solar System and the Universe.',
+				img: "news/news_0001/back.jpg",
+				alt: "Earth",
+				seen: false
+			},
+		}
+	},
+	methods: {
+		show_content: function(news) {
+			news.seen = !news.seen
+			// this.seen = !this.seen
+		}
 	}
 })
